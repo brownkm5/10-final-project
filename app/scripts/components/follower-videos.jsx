793f93cee59b7f1e73abf48ff736d35ce14fa47c
Kevin Brown <brownkm531@gmail.com>
2017-03-24 08:32:10 -0400

    bug fixes

--- app/scripts/components/follower-videos.jsx
+++ app/scripts/components/follower-videos.jsx
@@ -126,7 +126,7 @@ var FollowerVideoContainer = React.createClass({
     var userToken = token;
     $.ajaxSetup({
       beforeSend: function(xhr){
-        xhr.setRequestHeader('X-Auth', userToken);
+        xhr.setRequestHeader('X-Auth', da4b685574e5546dbd34e64ed1e6c8c7946435d7);
       }
     });
   },
